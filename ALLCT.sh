4815f294f36b45463b4eb019ee0622b6aed1e131
rokSkr <68292269+rokSkr@users.noreply.github.com>
2021-01-02 07:01:08 +0000

    Update ALLCT.sh

--- ALLCT.sh
+++ ALLCT.sh
@@ -1,11 +1,11 @@
 termux-setup-storage
 apt update
-apt upgrade -y -n
+apt upgrade -y&& -n
 apt install git
 apt install bash
 apt install python -y
 apt install python 2 -y
 pkg install man
 apt install openssh
-apt install vim
+apt install vim - y
 
