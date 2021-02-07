ae1c234c9ce44379a786081c1e0852517f77738a
rokSkr <68292269+rokSkr@users.noreply.github.com>
2021-02-07 18:25:58 +0000

    Update ALLCT.sh

--- ALLCT.sh
+++ ALLCT.sh
@@ -1,20 +1,52 @@
-Print("I-T-S-C")
+echo -e "---I---T---S---C---"
+echo -e "Вылаю провал на память устройства(для полноценной установки некоторых библиотек)"
 termux-setup-storage
+echo -e "Обновляю пакеты Termux..."
 apt update && apt upgrade -y&& -n
+echo -e "Устанавливаются git..."
 apt install git
+echo -e "Устанавливаются bash..."
 apt install bash
+echo -e "Устанавливаются python..."
 apt install python -y
-apt install python 2 -y
+echo -e "Устанавливаются python v2..."a
+pt install python 2 -y
+echo -e "Устанавливаются man..."
 pkg install man
+echo -e "Устанавливаются openssh..."
+
 apt install openssh
+echo -e "Устанавливаются vim..."
+
 apt install vim - y
+echo -e "Устанавливаются tor..."
+
 apt install tor -y
+echo -e "Устанавливаются tor..."
+
 apk install wget
+echo -e "Устанавливаются figlet..."
+
 pkg install figlet
+echo -e "Устанавливаются curl..."
+
 pkg install curl
+echo -e "Устанавливаются php..."
+
 pkg install php
+echo -e "Устанавливаются jq..."
+
 apt install jq
+echo -e "Устанавливаются perl..."
+
 apt install perl
+echo -e "Устанавливаются dart..."
+
 apt install dart
+echo -e "Устанавливаются nano..."
+
 apt install nano
+echo -e "Устанавливаются nmap..."
+
 apt install nmap
+echo -e "Установка всех комплектов произошла успешно!"
